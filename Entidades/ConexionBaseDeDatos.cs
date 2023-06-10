484e711453d32f49d40f45b8f57967b477d56844
YoberMarlow29 <yober2828@gmail.com>
2023-06-10 17:05:32 -0300

    commit

---
+++ Entidades/ConexionBaseDeDatos.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Entidades
+{
+    public class ConexionBaseDeDatos
+    {
+        private static string cadena_conexion;
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        static ConexionBaseDeDatos()
+        {
+            SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+            builder.DataSource = ".";
+            builder.IntegratedSecurity = true;
+            //builder.UserID = "usuario";
+            //builder.Password = "clave";
+            builder.InitialCatalog = "GeneralaDataBase";
+            ConexionBaseDeDatos.cadena_conexion = builder.ConnectionString;
+        }
+        public ConexionBaseDeDatos()
+        {
+            // CREO UN OBJETO SQLCONECTION
+            this.conexion = new SqlConnection(ConexionBaseDeDatos.cadena_conexion);
+        }
+        public bool ProbarConexion()
+        {
+            bool rta = true;
+
+            try
+            {
+                this.conexion.Open();
+            }
+            catch (Exception)
+            {
+                rta = false;
+            }
+            finally
+            {
+                if (this.conexion.State == ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return rta;
+        }
+        public List<Usuario> ObtenerListaDato()
+        {
+            List<Usuario> lista = new List<Usuario>();
+
+            try
+            {
+                this.comando = new SqlCommand();
+
+                this.comando.CommandType = CommandType.Text;
+                this.comando.CommandText = "SELECT * FROM Usuario";
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                this.lector = comando.ExecuteReader();
+
+                while (lector.Read())
+                {
+                    Usuario item = new Usuario();
+
+                    // ACCEDO POR NOMBRE, POR INDICE O POR GETTER (SEGUN TIPO DE DATO)
+                    item.Id = (int)lector["Id"];
+                    item.Nombre = lector["Nombre"].ToString();
+                    item.Apellido = lector["Apellido"].ToString();
+                    item.Correo = lector["Correo"].ToString();
+                    item.Clave = lector["Clave"].ToString();
+                    item.PartidasJugadas = int.Parse(lector["PartidasJugadas"].ToString());
+                    item.PartidasGanadas = int.Parse(lector["PartidasGanadas"].ToString());
+                    item.PartidasPerdidas = int.Parse(lector["PartidasPerdidas"].ToString());
+
+                    lista.Add(item);
+                }
+
+                lector.Close();
+
+            }
+            catch (Exception)
+            {
+                return null;
+            }
+            finally
+            {
+                if (this.conexion.State == ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return lista;
+        }
+        public bool AgregarDato(Usuario param)
+        {
+            bool rta = true;
+
+            try
+            {
+                string sql = "INSERT INTO dbo.tabla_jugadores (Nombre, Apellido, Correo, Clave, PartidasJugadas, PartidasGanadas, PartidasPerdidas) VALUES(";
+                sql = sql + "'" + param.Nombre + "','" + param.Apellido + "','" + param.Correo + "','" + param.Clave + "'," +
+                param.PartidasJugadas.ToString() + "," + param.PartidasGanadas.ToString() + "," + param.PartidasPerdidas.ToString() + ")";
+
+                this.comando = new SqlCommand();
+
+                this.comando.CommandType = CommandType.Text;
+                this.comando.CommandText = sql;
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                int filasAfectadas = this.comando.ExecuteNonQuery();
+
+                if (filasAfectadas == 0)
+                {
+                    rta = false;
+                }
+            }
+            catch (Exception)
+            {
+                rta = false;
+            }
+            finally
+            {
+                if (this.conexion.State == ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return rta;
+        }
+
+        public bool ModificarDato(Usuario param)
+        {
+            bool rta = true;
+
+            try
+            {
+                this.comando = new SqlCommand();
+
+                this.comando.Parameters.AddWithValue("@Id", param.Id);
+                this.comando.Parameters.AddWithValue("@Nombre", param.Nombre);
+                this.comando.Parameters.AddWithValue("@Apellido", param.Apellido);
+                this.comando.Parameters.AddWithValue("@Correo", param.Correo);
+                this.comando.Parameters.AddWithValue("@Clave", param.Clave);
+                this.comando.Parameters.AddWithValue("@PartidasJugadas", param.PartidasJugadas);
+                this.comando.Parameters.AddWithValue("@PartidasGanadas", param.PartidasGanadas);
+                this.comando.Parameters.AddWithValue("@PartidasPerdidas", param.PartidasPerdidas);
+
+                string sql = "UPDATE Usuario ";
+                sql += "SET Nombre = @Nombre, Apellido = @Apellido, Correo = @Correo, Clave = @Clave, PartidasJugadas = @PartidasJugadas, PartidasGanadas = @PartidasGanadas,  PartidasPerdidas = @PartidasPerdidas ";
+                sql += "WHERE id = @id";
+
+                this.comando.CommandType = CommandType.Text;
+                this.comando.CommandText = sql;
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                int filasAfectadas = this.comando.ExecuteNonQuery();
+
+                if (filasAfectadas == 0)
+                {
+                    rta = false;
+                }
+
+            }
+            catch (Exception)
+            {
+                rta = false;
+            }
+            finally
+            {
+                if (this.conexion.State == ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+            return rta;
+        }
+        public bool EliminarDato(int id)
+        {
+            bool rta = true;
+
+            try
+            {
+                this.comando = new SqlCommand();
+
+                this.comando.Parameters.AddWithValue("@Id", id);
+
+                string sql = "DELETE FROM Usuario ";
+                sql += "WHERE Id = @Id";
+
+                this.comando.CommandType = CommandType.Text;
+                this.comando.CommandText = sql;
+                this.comando.Connection = this.conexion;
+
+                this.conexion.Open();
+
+                int filasAfectadas = this.comando.ExecuteNonQuery();
+
+                if (filasAfectadas == 0)
+                {
+                    rta = false;
+                }
+
+            }
+            catch (Exception)
+            {
+                rta = false;
+            }
+            finally
+            {
+                if (this.conexion.State == ConnectionState.Open)
+                {
+                    this.conexion.Close();
+                }
+            }
+
+            return rta;
+        }
+    }
+}
---
+++ Entidades/Usuario.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellido;
+        private string correo;
+        private string clave;
+        private int partidasGanadas;
+        private int partidasPerdidas;
+        private int partidasJugadas;
+        public int Id { get => id; set => id = value; }
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public string Correo { get => correo; set => correo = value; }
+        public string Clave { get => clave; set => clave = value; }
+        public int PartidasGanadas { get => partidasGanadas; set => partidasGanadas = value; }
+        public int PartidasPerdidas { get => partidasPerdidas; set => partidasPerdidas = value; }
+        public int PartidasJugadas { get => partidasJugadas; set => partidasJugadas = value; }
+        public Usuario() 
+        {
+            this.nombre = "";
+            this.apellido = "";
+            this.correo = "";
+            this.clave = "";
+            this.partidasGanadas = 0;
+            this.partidasPerdidas = 0;
+            this.partidasJugadas = 0;
+        }
+        public Usuario(string nombre, string apellido, string correo, string clave, int partidasGanadas, int partidasPerdidas, int partidasJugadas):this()
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.correo = correo;
+            this.clave = clave;
+            this.partidasGanadas = partidasGanadas;
+            this.partidasPerdidas = partidasPerdidas;
+            this.partidasJugadas = partidasJugadas;
+        }
+        public Usuario(int id, string nombre, string apellido, string correo, string clave, int partidasGanadas, int partidasPerdidas, int partidasJugadas):this(nombre,apellido,correo,clave,partidasGanadas,partidasPerdidas,partidasJugadas)
+        {
+            this.id = id;
+        }
+
+        public bool ComprobarClave(string clave)
+        {
+            return clave == this.clave;
+        }
+
+    }
+}
