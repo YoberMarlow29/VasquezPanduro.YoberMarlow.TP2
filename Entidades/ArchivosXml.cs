fd3904240b41a3b333fc9f0b3d38bf3efddb8cd5
YoberMarlow29 <yober2828@gmail.com>
2023-06-12 18:28:05 -0300

    commit

---
+++ Entidades/ArchivosXml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class ArchivosXml<T> : IArchivos<T> where T : class, new()
+    {
+        public bool Serializar(T obj, string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Deserializar(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ Entidades/IArchivos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public interface IArchivos<T> where T : class, new()
+    {
+        public bool Serializar(T obj,string path);
+        public T Deserializar(string path);
+    
+    }
+}
