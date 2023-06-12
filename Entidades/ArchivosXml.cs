675119e0d23f69d7e0c16a0535723eb5b5a34369
YoberMarlow29 <yober2828@gmail.com>
2023-06-12 18:40:06 -0300

    serializadores listos

--- Entidades/ArchivosXml.cs
+++ Entidades/ArchivosXml.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace Entidades
 {
@@ -10,12 +12,50 @@ namespace Entidades
     {
         public bool Serializar(T obj, string path)
         {
-            throw new NotImplementedException();
+            bool retorno=false;
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+
+                using (FileStream fileStream = new FileStream(path, FileMode.Create))
+                {
+                    serializer.Serialize(fileStream, obj);
+                    retorno = true;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al serializar la lista en formato XML y guardar en el archivo '" + path + "': " + ex.Message, ex);
+                retorno = false;
+            }
+            return retorno;
         }
 
         public T Deserializar(string path)
         {
-            throw new NotImplementedException();
+            T aux = new T();
+            try
+            {
+                if (File.Exists(path))
+                {
+                    XmlSerializer serializer = new XmlSerializer(typeof(List<T>));
+
+                    using (FileStream fileStream = new FileStream(path, FileMode.Open))
+                    {
+                        List<T> lista = (List<T>)serializer.Deserialize(fileStream);
+                        return aux;
+                    }
+                }
+                else
+                {
+                    File.WriteAllText(path, string.Empty);
+                    return new T();
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error al deserializar el archivo XML '" + path + "': " + ex.Message, ex);
+            }
         }
 
 
